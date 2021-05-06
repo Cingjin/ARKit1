f27e4508a3579d050131224cac077cb846676f27
yuxingjin <15327288437@163.com>
2021-05-06 15:50:16 +0800

    1、立方体提交

--- ARKit1/AppDelegate.swift
+++ ARKit1/AppDelegate.swift
@@ -14,10 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
-        window = UIWindow.init(frame: UIScreen.main.bounds)
-        window?.rootViewController = ViewController()
-        window?.makeKeyAndVisible()
+        
         return true
     }
 
