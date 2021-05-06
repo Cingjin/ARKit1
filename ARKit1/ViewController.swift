9ba026cec17f96c2e79db6423d799f5debe9d7f3
yuxingjin <15327288437@163.com>
2021-05-06 15:00:02 +0800

    1、提交

---
+++ ARKit1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ARKit1
+//
+//  Created by Anmo on 2021/5/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+    }
+}
